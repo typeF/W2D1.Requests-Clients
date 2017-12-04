f97c907b3349c3fd0b4c644eb55845496a69afa4
Frank Lam <frank.is.missing@gmail.com>
2017-12-04 22:46:15 +0000

    Removed comment instructions

--- requesting.js
+++ requesting.js
@@ -9,14 +9,3 @@ console.log("Response headers: " + response.headers['content-type']);
 console.log("Downloading image...");
 }).on('end', function(){
 console.log("Download complete.");}).pipe(fs.createWriteStream('./future.jpg'));
-
-
-
-// Create a new .js file and using request, make a 'GET' request to 'https://sytantris.github.io/http-examples/future.jpg' using chaining. The chain should:
-// Use .on('error') to handle any errors
-// Use .on('response') to log the HTTPS response code, response message (response.statusMessage) and content type (response.headers['content-type'])
-// Use piping and fs.createWriteStream to save the file to your working directory ('./future.jpg')
-
-// If you wanted to improve user experience, you might wish to let the user know what is happening, perhaps with two console.log statements:
-// console.log('Downloading image...');
-// console.log('Download complete.');
